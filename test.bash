06cff1e5bbcc82d5d6685da8fbfa651be9c0f581
Taisyu Shibata <t_shiba117@yahoo.co.jp>
2022-11-05 21:37:00 +0900

    Add tests

--- test.bash
+++ test.bash
@@ -11,7 +11,16 @@ res=0
 
 ###I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng $LINENO
+[ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]       || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]       || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
